e3b438fb06715ec8b22a7cfa7ab603e8822f9a38
CNurt <curtseeling@hotmail.de>
2016-02-20 08:12:13 +0100

    first upload

---
+++ JS_MoveThings.js
@@ -0,0 +1,57 @@
+﻿//from here: http://www.petercollingridge.co.uk/book/export/html/437
+var pt   = svg.createSVGPoint();
+
+var selectedElement = 0;
+var currentX = 0;
+var currentY = 0;
+var currentMatrix = 0;
+
+function selectElement(evt) {
+  selectedElement = evt.target;
+	//function cursorPoint(evt) could be used here, but didn't get it to work yet
+  //pt.x = evt.clientX; pt.y = evt.clientY;
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+  currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
+
+  for(var i=0; i<currentMatrix.length; i++) {
+	currentMatrix[i] = parseFloat(currentMatrix[i]);
+  }
+  
+  selectedElement.setAttributeNS(null, "onmousemove", "moveElement(evt)");
+  selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)");
+  selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");
+}
+	
+function moveElement(evt) {
+  var dx = evt.clientX - currentX;
+  var dy = evt.clientY - currentY;
+  currentMatrix[4] += dx;
+  currentMatrix[5] += dy;
+  
+  selectedElement.setAttributeNS(null, "transform", "matrix(" + currentMatrix.join(' ') + ")");
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+}
+	
+function deselectElement(evt) {
+  if(selectedElement != 0){
+	  selectedElement.removeAttributeNS(null, "onmousemove");
+	  selectedElement.removeAttributeNS(null, "onmouseout");
+	  selectedElement.removeAttributeNS(null, "onmouseup");
+	  selectedElement = 0;
+	  }
+	}
+
+
+//IMPROVEMENT POSSIBLE
+//http://phrogz.net/svg/rotate-to-point-at-cursor.svg
+//http://stackoverflow.com/questions/10298658/mouse-position-inside-autoscaled-svg
+// Get point in global SVG space
+/*
+function cursorPoint(evt){
+	//HIER ist der Fehler, irgenwie funktioniert das mit der Variable pt.x nicht
+	pt.x = evt.clientX; pt.y = evt.clientY;
+	return pt.matrixTransform(svg.getScreenCTM().inverse());
+}
+*/
---
+++ JavascriptForSVG.js
@@ -0,0 +1,69 @@
+﻿//List of Events:  http://www.w3schools.com/jsref/dom_obj_event.asp
+var iTest = 1;
+//var TestStringArray = [ "erstes Testelement\n" ]; 
+var TestString = "erstes Testelement;闺女\n"
+
+//It would be better to use .addEventListener but I didn't get it to work
+//so I'll stick with the  onclick=" function" in the SVG
+// see following for events:  https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+
+//mit Rückgabewert
+var fOnMouseOverProvince = function(ThisElement) {
+//ohne Rückgabewert
+//function fOnMouseOverProvince(ThisElement) {
+	//ThisElement.setAttribute('fill', '#FFD026');
+}
+
+var fOnClickProvince = function(ThisElement) {
+	//Testing the TextBox
+	changeText();
+	onChangeText(document.getElementById("TaskText"), document.getElementById("TaskBox"));
+	
+	//Testing fileoutput and stringarray
+	TestString = TestString + ThisElement.id + ";" + prompt(ThisElement.id) + ";\n"
+}
+
+var fOnMouseOutProvince = function(ThisElement) {
+	//ThisElement.setAttribute('fill', '#FFF6D5');
+}
+
+function changeText() {
+// ATM just TEST
+	iTest = iTest * (iTest + 1);
+	document.getElementById("TaskText").innerHTML = iTest;
+}
+
+function onChangeText(TextID, RectangleID) {
+// Resize rectangle used as a textbox in order to keep text inside the box
+	var CurrentTextWidth = TextID.getBoundingClientRect().width;
+	var CurrentTextHeight = TextID.getBoundingClientRect().height;
+	
+	RectangleID.setAttribute('width', CurrentTextWidth+20);
+	RectangleID.setAttribute('height', CurrentTextHeight+20);
+}
+
+function onTextfileClick() {
+    var link = document.getElementById('Link_LittleTextBox');
+		link.setAttribute('xlink:href', makeTextFile(TestString));
+    //link.href = makeTextFile("Ni Hao!");
+		//prompt(makeTextFile("Ni Hao!"));
+    //link.style.display = 'block';
+  }
+
+makeTextFile = function (text) {
+	var textFile = null
+	//charset=utf-8 works only in firefox so far
+	var data = new Blob([text], {type: 'text/plain; charset=utf-8'});
+
+	// If we are replacing a previously generated file we need to
+	// manually revoke the object URL to avoid memory leaks.
+	if (textFile !== null) {
+		window.URL.revokeObjectURL(textFile);
+	}
+
+	textFile = window.URL.createObjectURL(data);
+
+	// returns a URL you can use as a href
+	return textFile;
+};
