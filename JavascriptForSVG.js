b4208e7d2236aefc9f1afe91465485ed3601dcda
CS <CS>
2021-11-17 15:30:44 +0100

    refactoring, created classes 'MapofChina' & 'GUI'

--- JavascriptForSVG.js
+++ JavascriptForSVG.js
@@ -2,13 +2,6 @@
 //List of Events:  http://www.w3schools.com/jsref/dom_obj_event.asp
 //Helpful for JScript Debugging: fchangeText(currentMatrix);//alert(ElTouched + ElMove + "\n" + dx + "\n" + dy);
 var NrClicks = 0;   //Amount of clicks on Provinces
-//var TestStringArray = [ "erstes Testelement\n" ]; 
-var TestString = "erstes Testelement;闺女\n";
-var mouseMoved = false;
-var mouseDown = false;
-var activeProvince = -1;
-var hasmoved = false;
-var scale = Math.log(1.0);
 const provinces = new Provinces();
 
 // see following for events:  https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
@@ -18,116 +11,13 @@ window.addEventListener("load", finit, false);
 // window.addEventListener("DOMContentLoaded", finit, false);
 
 function finit() {
-	fAddEventListeners();
-	document.getElementById("WholeMap").setAttribute("transform", "matrix(1 0 0 1 0 0)");
-	fResizeTextBox(document.getElementById("TaskText"), document.getElementById("TaskBox"));
-
-
+	this.mapobject = new MapofChina(receiveMapTileId);
+	this.guiobject = new GUI();
 }
 
-function fAddEventListeners() {
-	document.addEventListener("mousedown", function () {
-		mouseDown = true;
-	}, false);
-	document.addEventListener("mouseup", function () {
-		mouseDown = false;
-		hasmoved = false;
-	}, false);
-	document.addEventListener("mouseleave", function () {
-		activeProvince = -1;
-	});
-	document.addEventListener("mousemove", fMovement, false);
-	document.addEventListener("wheel", fZoom, { passive: false });
-
-	var MapTilePaths = document.getElementById("MapTiles").getElementsByTagName("path");
-	var WholeMapPaths = document.getElementById("WholeMap").getElementsByTagName("path");
-	var limit1 = MapTilePaths.length;
-	for (i = 0; i < limit1; i++) {
-		MapTilePaths[i].addEventListener("mousedown", fClickMapTile, false);
-		MapTilePaths[i].addEventListener("mouseup", fReleaseMapTile, false);
-	}
-	var limit2 = WholeMapPaths.length;
-	for (i = limit1; i < limit2; i++) {
-	}
-}
-
-function fZoom(evt) {
-	evt.preventDefault();
-	let WholeMap = document.getElementById("WholeMap")
-
-	scale += evt.deltaY * -0.001;
-
-	// Restrict scale
-	scale = Math.min(Math.max(Math.log(.25), scale), Math.log(4));
-
-	// Apply scale transform
-	realscale = Math.E ** (scale);
-	WholeMap.setAttribute("transform", `scale(${realscale})`);
-	// for looking up the real values (in x and y) from the element:
-	// WholeMap.transform.baseVal[0].matrix.a
-	// WholeMap.transform.baseVal[0].matrix.d
-}
-
-function fMovement(evt) {
-	if (mouseDown) {
-		if (!hasmoved) {
-			fDragMap(evt)
-		}
-		hasmoved = true;
-	}
-}
-
-// TODO: realize zooming with transform matrix only on "WholeMap" instead of each path individually. This also needs changing of fZoom()
-function fDragMap(evt) {
-	//IMPROVEMENT POSSIBLE, TODO
-	// Lock MousePointer and make invisible with following functions:
-	//  https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
-
-	//move the map by clicking on any map tile
-	document.getElementById('MapCover').setAttribute("visibility", "visible !important");
-	selectOtherElement(document.getElementById('MapCover'), document.getElementById('WholeMap').getElementsByTagName('path'), true);
-	return false;
-}
-
-//mit Rückgabewert
-//var fOnMouseOverProvince = function(ThisElement) {
-//ohne Rückgabewert
-function fOnMouseOverProvince(ThisElement) {
-	//ThisElement.setAttribute('fill', '#FFD026');
-}
-
-function fClickMapTile(evt) {
-	activeProvince = fGetMapTileID(evt.target);
-}
-
-function fGetMapTileID(el) {
-	classname = el.className.baseVal;
-	if (classname != "province") {
-		classobject = ffindUpperClass(el, "province");
-		if (classobject === null) {
-			return null
-		}
-		else {
-			id = classobject.id;
-		}
-	}
-	else {
-		id = el.id;
-	}
-	return id;
-}
-
-function fReleaseMapTile(evt) {
-	currentprovinceid = fGetMapTileID(evt.target);
-	if (activeProvince == currentprovinceid) {
-		fRegisterMapTile(currentprovinceid)
-		hasmoved = false;
-	}
-}
-
-function fRegisterMapTile(id) {
-	NrClicks = NrClicks + 1;
-	fchangeText(fgetProvinceInfo(id, 'ProvinceSIM'));
+function receiveMapTileId(id) {
+	++NrClicks;
+	guiobject.fchangeText(fgetProvinceInfo(id, 'ProvinceSIM'));
 	return false;
 }
 
@@ -135,25 +25,6 @@ function fgetProvinceInfo(id, what) {
 	// id   ...  id of SVG element of province
 	// what ...  which field to choose (i.e. ID;ProvinceEN;ProvinceSIM;ProvincePIN;CapitalEN;CapitalSIM;CapitalPIN;Abbreviation)
 	return provinces[id][what];
-}
-
-function fOnMouseOutProvince(ThisElement) {
-	//ThisElement.setAttribute('fill', '#FFF6D5');
-}
-
-function fchangeText(text) {
-	// ATM just TEST
-	document.getElementById("TaskText").innerHTML = text;
-	fResizeTextBox(document.getElementById("TaskText"), document.getElementById("TaskBox"));
-}
-
-function fResizeTextBox(TextID, RectangleID) {
-	// Resize rectangle used as a textbox in order to keep text inside the box
-	var CurrentTextWidth = TextID.getBoundingClientRect().width;
-	var CurrentTextHeight = TextID.getBoundingClientRect().height;
-
-	RectangleID.setAttribute('width', CurrentTextWidth);
-	RectangleID.setAttribute('height', CurrentTextHeight);
 }
 
 function ffindUpperClass(el, classname) {
@@ -165,4 +36,156 @@ function ffindUpperClass(el, classname) {
 		}
 	}
 	return null;
+}
+
+
+
+class MapofChina {
+	mouseMoved = false;
+	mouseDown = false;
+	activeProvince = -1;
+	hasmoved = false;
+	scale = Math.log(1.0);
+
+	constructor(mapTileIdReceiverFunction) {
+		this.finit();
+		this.mapTileIdReceiverFunction = mapTileIdReceiverFunction;
+	}
+
+	finit() {
+		this.fAddEventListeners();
+		this.WholeMap = document.getElementById("WholeMap");
+		this.WholeMap.setAttribute("transform", "matrix(1 0 0 1 0 0)");
+	}
+
+	fAddEventListeners() {
+		// puttin EvenListeners from inside an class onto objects outside the class causes the 'this' keyword to be the event.target and not the class, that initially added the eventhandler
+		// see https://stackoverflow.com/questions/43727516/how-adding-event-handler-inside-a-class-with-a-class-method-as-the-callback/43727582#43727582
+
+		document.addEventListener("mousedown", function () {
+			this.mouseDown = true;
+		}.bind(this), false);
+		document.addEventListener("mouseup", function () {
+			this.mouseDown = false;
+			this.hasmoved = false;
+		}.bind(this), false);
+		document.addEventListener("mouseleave", function () {
+			this.activeProvince = -1;
+		}.bind(this));
+		document.addEventListener("mousemove", this.fMovement.bind(this), false);
+		document.addEventListener("wheel", this.fZoom.bind(this), { passive: false });
+
+		var MapTilePaths = document.getElementById("MapTiles").getElementsByTagName("path");
+		var limit1 = MapTilePaths.length;
+		for (var i = 0; i < limit1; i++) {
+			MapTilePaths[i].addEventListener("mousedown", this.fClickMapTile.bind(this), false);
+			MapTilePaths[i].addEventListener("mouseup", this.fReleaseMapTile.bind(this), false);
+		}
+	}
+
+	fZoom(evt) {
+		evt.preventDefault();
+
+		this.scale += evt.deltaY * -0.001;
+
+		// Restrict scale
+		this.scale = Math.min(Math.max(Math.log(.25), this.scale), Math.log(4));
+
+		// Apply scale transform
+		var realscale = Math.E ** (this.scale);
+		this.WholeMap.setAttribute("transform", `scale(${realscale})`);
+		// for looking up the real values (in x and y) from the element:
+		// WholeMap.transform.baseVal[0].matrix.a
+		// WholeMap.transform.baseVal[0].matrix.d
+	}
+
+	fMovement(evt) {
+		if (this.mouseDown) {
+			if (!this.hasmoved) {
+				this.fDragMap(evt)
+			}
+			this.hasmoved = true;
+		}
+	}
+
+	// TODO: realize zooming with transform matrix only on "WholeMap" instead of each path individually. This also needs changing of fZoom()
+	fDragMap(evt) {
+		//IMPROVEMENT POSSIBLE, TODO
+		// Lock MousePointer and make invisible with following functions:
+		//  https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+
+		//move the map by clicking on any map tile
+		var MapCover = document.getElementById('MapCover');
+		MapCover.setAttribute("visibility", "visible !important");
+		selectOtherElement(MapCover, this.WholeMap.getElementsByTagName('path'), true);
+		return false;
+	}
+
+	fOnMouseOverMapTile(ThisElement) {
+		//ThisElement.setAttribute('fill', '#FFD026');
+	}
+
+	fGetMapTileID(el) {
+		var classname = el.className.baseVal;
+		var id;
+		if (classname != "province") {
+			var classobject = ffindUpperClass(el, "province");
+			if (classobject === null) {
+				return null
+			}
+			else {
+				id = classobject.id;
+			}
+		}
+		else {
+			id = el.id;
+		}
+		return id;
+	}
+
+	fClickMapTile(evt) {
+		this.activeProvince = this.fGetMapTileID(evt.target);
+	}
+
+	fReleaseMapTile(evt) {
+		var currentprovinceid = this.fGetMapTileID(evt.target);
+		if (this.activeProvince == currentprovinceid) {
+			this.mapTileIdReceiverFunction(currentprovinceid)
+			// this.fRegisterMapTile(currentprovinceid)
+			this.hasmoved = false;
+		}
+	}
+
+	fOnMouseOutProvince(ThisElement) {
+		//ThisElement.setAttribute('fill', '#FFF6D5');
+	}
+}
+
+
+
+class GUI {
+	constructor() {
+		this.finit();
+	}
+
+	finit() {
+		this.TaskText_el = document.getElementById("TaskText");
+		this.TaskBox_el = document.getElementById("TaskBox");
+		this.fResizeTextBox(this.TaskText_el, this.TaskBox_el);
+	}
+
+	fchangeText(text) {
+		// ATM just TEST
+		this.TaskText_el.innerHTML = text;
+		this.fResizeTextBox(this.TaskText_el, this.TaskBox_el);
+	}
+
+	fResizeTextBox(TextID, RectangleID) {
+		// Resize rectangle used as a textbox in order to keep text inside the box
+		var CurrentTextWidth = TextID.getBoundingClientRect().width;
+		var CurrentTextHeight = TextID.getBoundingClientRect().height;
+
+		RectangleID.setAttribute('width', CurrentTextWidth);
+		RectangleID.setAttribute('height', CurrentTextHeight);
+	}
 }
